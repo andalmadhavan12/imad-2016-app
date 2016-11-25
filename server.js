01b51e0e77bdf060b10d301d1accb5ddc8f0a5f2
andalmadhavan12 <andalpriya.m@gmail.com>
2016-11-25 15:10:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article.one', function (req, res) {
+  res.send('This is first article');
+});
+
+app.get('/article.two', function (req, res) {
+  res.send('This is two article');
+});
+
+app.get('/article.three', function (req, res) {
+  res.send('This is three article');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
